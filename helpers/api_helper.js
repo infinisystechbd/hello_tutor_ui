47e2e91dfbd9f0f883316b06c6d84ae6604873f1
Rafi340 <rafisamnan340@gmail.com>
2023-05-19 00:22:13 +0600

    api helper

--- helpers/api_helper.js
+++ helpers/api_helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const API_URL = process.env.BASE_URL;
-
+const API_URL = process.env.NEXT_BASE_URL;
+console.log(API_URL);
 const axiosApi = axios.create({
     baseURL: API_URL
   });
