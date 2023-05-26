94115de8aafb2e14bb76e9230b4035b3122b9e98
Rafi340 <rafisamnan340@gmail.com>
2023-05-27 01:59:46 +0600

    feat: create hook for get all data and install react-query

---
+++ utils/hooks/useGetAllData.js
@@ -0,0 +1,13 @@
+import { get } from '@/helpers/api_helper';
+import { useQuery } from '@tanstack/react-query';
+
+export const useGetAllData = (queryKey , url) => {
+    const fetchData = (url) => {
+        return get(url);
+    }
+    return useQuery({
+        queryKey: [queryKey , url],
+        queryFn: ()=> fetchData(url),
+        keepPreviousData: true
+    } );
+}
