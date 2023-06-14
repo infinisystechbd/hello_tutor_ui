17591d9bfa848b80d17f3dea712d634ecac83141
Suparan <suparansharmap@gmail.com>
2023-06-14 23:13:44 +0600

    category not complected

--- constants/api_endpoints/categorieEndPoints.js
+++ constants/api_endpoints/categorieEndPoints.js
@@ -1,6 +1,6 @@
 export const CATEGORIE_END_POINT = {
     create: () => `/category`,
-    get: () => `/category`,
+    get: (search=null) => `/category?search=${search}`,
     info: (id) => `/category/${id}`,
     update: (id) => `/category/${id}`,
     delete:(id) => `/category/${id}`,
