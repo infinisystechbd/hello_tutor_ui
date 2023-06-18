c5eab719a01bd895036a3a98c7fdb5a12572c63a
Suparan <suparansharmap@gmail.com>
2023-06-18 22:16:36 +0600

    bug solve this

--- components/LeftSidebar.js
+++ components/LeftSidebar.js
@@ -1,47 +1,80 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { FileOutlined, PieChartOutlined, UserOutlined, DesktopOutlined, TeamOutlined } from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
 import { useRouter } from 'next/router';
-const LeftSidebar = () => {
-    const { Sider } = Layout;
-    const [collapsed, setCollapsed] = useState(false);
-    const router = useRouter();
-    function getItem(label, key, icon, children, path = '/') {
-        return {
-          key,
-          icon,
-          children,
-          label,
-          path,
-        };
-      }
-    const items = [
-        getItem('Option 1', '/modules/content', <PieChartOutlined />, null, '/modules/content'),
-        getItem('Option 2', '2', <DesktopOutlined />),
-        getItem('Master Data', 'sub1', <UserOutlined />, [
-            getItem('Subject', '/modules/hrmModalV/subject', null, null, '/modules/hrmModalV/subject'),
-            getItem('Class', '/modules/hrmModalV/class', null, null, '/modules/hrmModalV/class'),
-            getItem('Category', '/modules/hrmModalV/categorie', null, null, '/modules/hrmModalV/categorie'),
-            getItem('City', '/modules/hrmModalV/city', null, null, '/modules/hrmModalV/city'),
-            getItem('Location', '/modules/hrmModalV/location', null, null, '/modules/hrmModalV/location'),
-            getItem('Guardian', '/modules/hrmModalV/guardian', null, null, '/modules/hrmModalV/guardian'),
-            getItem('Tutor', '/modules/hrmModalV/tutor', null, null, '/modules/hrmModalV/tutor'),
-          ], '/users'),
-        getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-        getItem('Files', '9', <FileOutlined />),
-    ];
-    return (
-        <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          defaultSelectedKeys={[router.pathname]}
-          mode="inline"
-          onSelect={({ key }) => router.push(key)}
-          items={items}
-        />
-      </Sider>
-    )
-}
 
-export default LeftSidebar
+const { Sider } = Layout;
+
+const LeftSidebar = () => {
+  const router = useRouter();
+  const [collapsed, setCollapsed] = useState(false);
+
+  const items = [
+    {
+      key: '/modules/content',
+      icon: <PieChartOutlined />,
+      label: 'Option 1',
+      path: '/modules/content',
+    },
+    {
+      key: '/modules/hrmModalV',
+      icon: <UserOutlined />,
+      label: 'Master Data',
+      children: [
+        { key: '/modules/hrmModalV/subject', label: 'Subject' },
+        { key: '/modules/hrmModalV/class', label: 'Class' },
+        { key: '/modules/hrmModalV/categorie', label: 'Category' },
+        { key: '/modules/hrmModalV/city', label: 'City' },
+        { key: '/modules/hrmModalV/location', label: 'Location' },
+        { key: '/modules/hrmModalV/guardian', label: 'Guardian' },
+        { key: '/modules/hrmModalV/tutor', label: 'Tutor' },
+      ],
+    },
+    {
+      key: '/users',
+      icon: <TeamOutlined />,
+      label: 'Team',
+      children: [
+        { key: '6', label: 'Team 1' },
+        { key: '8', label: 'Team 2' },
+      ],
+    },
+    { key: '9', icon: <FileOutlined />, label: 'Files' },
+  ];
+
+  const renderMenuItems = (menuItems) => {
+    return menuItems.map((item) => {
+      if (item.children) {
+        return (
+          <Menu.SubMenu key={item.key} icon={item.icon} title={item.label}>
+            {renderMenuItems(item.children)}
+          </Menu.SubMenu>
+        );
+      }
+      return (
+        <Menu.Item key={item.key} icon={item.icon}>
+          {item.label}
+        </Menu.Item>
+      );
+    });
+  };
+
+
+ 
+
+  return (
+    <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed} className="fixed-sidebar">
+      <div className="demo-logo-vertical" />
+      <Menu
+        theme="dark"
+        defaultSelectedKeys={[router.pathname]}
+        mode="inline"
+        onSelect={({ key }) => router.push(key)}
+      >
+        {renderMenuItems(items)}
+      </Menu>
+    </Sider>
+  );
+};
+
+export default LeftSidebar;
