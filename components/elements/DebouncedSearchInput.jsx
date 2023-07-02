8f05671d75f0378d25ff838db774f63f8087fae9
Rafi340 <rafisamnan340@gmail.com>
2023-07-02 12:47:39 +0600

    feat: search issue solve

--- components/elements/DebouncedSearchInput.jsx
+++ components/elements/DebouncedSearchInput.jsx
@@ -1,24 +1,21 @@
+import { Input } from 'antd';
+import { debounce } from 'lodash';
+import { useCallback } from 'react';
 
-export default function ViewIcon() {
-    return (
-      <div className="icon">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width={24}
-          height={24}
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="feather feather-eye feather-sm"
-        >
-          <title>View</title>
-          <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-          <circle cx="12" cy="12" r="3"></circle>
-        </svg>
-      </div>
-    );
-  }
-  
+const DebouncedSearchInput = (props) => {
+  const { placeholder, onChange, delay, ...rest } = props;
+
+  const searchFieldHandler = (e) => {
+    onChange(e.target.value);
+  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debounceSearch = useCallback(debounce(searchFieldHandler, delay || 500), []);
+
+  return (
+    <>
+      <Input type="text"  placeholder={placeholder || ''} onChange={debounceSearch} {...rest} />
+    </>
+  );
+};
+
+export default DebouncedSearchInput;
