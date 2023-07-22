f30aa174795c1540f610ce27e92a061d18f67a88
Suparan <suparansharmap@gmail.com>
2023-07-22 21:23:55 +0600

    solved

--- pages/register.jsx
+++ pages/register.jsx
@@ -148,7 +148,7 @@ const LoginPage = () => {
                 <div className="container-fluid h-custom">
                     <div className="row d-flex justify-content-center align-items-center h-100">
                         <div className="col-md-8 col-lg-6 col-xl-5">
-                            <img
+                            <Image
                                 src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
                                 className="img-fluid"
                                 alt="Sample image"
