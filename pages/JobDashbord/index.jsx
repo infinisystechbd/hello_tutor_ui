73b2dfbba47ed32e64281bd2d4d857ce05d83e66
Rafi340 <rafisamnan340@gmail.com>
2024-01-24 21:29:40 +0600

    filter

--- pages/JobDashbord/index.jsx
+++ pages/JobDashbord/index.jsx
@@ -1,9 +1,8 @@
 import JobCard from "@/components/JobCard";
-import { DASHBOARD_END_POINT } from "@/constants";
-import { QUERY_KEYS } from "@/constants/queryKeys";
+import { CATEGORIE_END_POINT, DASHBOARD_END_POINT } from "@/constants";
 import { get } from "@/helpers/api_helper";
-import { useGetAllData } from "@/utils/hooks/useGetAllData";
-import Link from "next/link";
+import { mapArrayToDropdown } from "@/helpers/common_Helper";
+import { Button, Card, Drawer } from "@material-tailwind/react";
 import { useCallback, useEffect, useState } from "react";
 
 const JobDashboard = () => {
@@ -14,18 +13,121 @@ const JobDashboard = () => {
   const [limit, setLimit] = useState(9);
   const [fromDate, SetFromDate] = useState();
   const [toDate, SetToDate] = useState();
+  const [category, setCategory] = useState([]);
+  const [selectedCateGory, setSelectedCategory] = useState("");
+  const [cityDropDown, setCityDropDown] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("");
+  const [locationDropDown, setLocationDropDown] = useState([]);
+  const [selectedLocation, setselectedLocation] = useState("");
+  const [classDropDown, setClassDropDown] = useState([]);
+  const [selectedClass, setSelectedClass] = useState("");
+  const [subjectDropDown, setSubjectDropDown] = useState([]);
+  const [selectedSubject, setSelectedSubject] = useState("");
+  const [tutionType, setTutionType] = useState("");
+  const [studentGender, setStudentGender] = useState("");
+  const [tutorGender, setTutorGender] = useState("");
+  const [openTop, setOpenTop] = useState(false);
+  const openDrawerTop = () => setOpenTop(true);
+  const closeDrawerTop = () => setOpenTop(false);
 
   const getAllData = async (limit, page) => {
     try {
-      const res = await get(DASHBOARD_END_POINT.dashbord(true, limit, page));
+      const res = await get(
+        DASHBOARD_END_POINT.dashbord(
+          true,
+          limit,
+          page,
+          fromDate,
+          toDate,
+          tutionType,
+          selectedCity,
+          selectedLocation,
+          selectedCateGory,
+          selectedClass,
+          selectedSubject,
+          studentGender,
+          tutorGender
+        )
+      );
       setDashboard((prevData) => [...prevData, ...res?.data]);
-    //   setDashboard(res?.data);
+      //   setDashboard(res?.data);
 
       setLoading(false);
     } catch (err) {
       console.log("Server Error ~!");
     }
   };
+  const fetchCategory = async () => {
+    const fetchdata = await get(CATEGORIE_END_POINT.get(1, -1, "", true));
+    const arrayToDrop = mapArrayToDropdown(fetchdata?.data, "name", "_id");
+    setCategory(arrayToDrop);
+  };
+  const onChangeFromDate = (date, dateString) => {
+    SetFromDate(dateString);
+  };
+  const onChangeToDate = (date, dateString) => {
+    SetToDate(dateString);
+  };
+  const onChangeCity = async (id) => {
+    setSelectedCity(id);
+    const fetchLocation = await get(LOCATION_END_POINT.getLocationByCityId(id));
+    const locationArrayToDropDown = mapArrayToDropdown(
+      fetchLocation?.data,
+      "name",
+      "_id"
+    );
+    setLocationDropDown(locationArrayToDropDown);
+  };
+  const onChangeLocation = (id) => {
+    setselectedLocation(id);
+  };
+  const onChangeCategory = (value) => {
+    setSelectedCategory(value);
+  };
+
+  const fetchCity = async () => {
+    const dataFetch = await get(CITY_END_POINT.get(1, -1, "", ""));
+    const cityArrayToDropDown = mapArrayToDropdown(
+      dataFetch?.data,
+      "name",
+      "_id"
+    );
+    setCityDropDown(cityArrayToDropDown);
+  };
+  const fetchClass = async () => {
+    const classFetch = await get(CLASS_END_POINT.get(1, -1, "", ""));
+    const classArrayToDropDown = mapArrayToDropdown(
+      classFetch?.data,
+      "name",
+      "_id"
+    );
+    setClassDropDown(classArrayToDropDown);
+  };
+  const onChangeClass = (value) => {
+    setSelectedClass(value);
+  };
+  const fetchSubject = async () => {
+    const subjectFetch = await get(SUBJECT_END_POINT.dropdown(1, -1, "", ""));
+    const subjectArrayToDropDown = mapArrayToDropdown(
+      subjectFetch?.data,
+      "name",
+      "_id"
+    );
+    setSubjectDropDown(subjectArrayToDropDown);
+  };
+  const onChangeSubject = (value) => {
+    setSelectedSubject(value);
+  };
+
+  const onChangeTutorGender = (value) => {
+    setTutorGender(value);
+  };
+  const onChangeStudentGender = (value) => {
+    setStudentGender(value);
+  };
+  useEffect(() => {
+    fetchCategory();
+  }, []);
 
   const handleIntersection = useCallback(
     (entries) => {
@@ -55,13 +157,48 @@ const JobDashboard = () => {
     };
   }, [handleIntersection]);
 
-  useEffect(() => {
+  /*  useEffect(() => {
     // Load initial data
     getAllData(limit, page);
-  }, [limit, page]);
+  }, [limit, page]); */
+  useEffect(() => {
+    getAllData(limit, page);
+  }, [limit, page, selectedCateGory]);
+  const resetFilter = () => {
+    SetFromDate("");
+    SetToDate("");
+    setSelectedCity("");
+    setselectedLocation("");
+    setSelectedClass("");
+    setSelectedSubject("");
+    setTutionType("");
+    setSelectedCategory("");
+    getAllData(limit, page);
+  };
 
   return (
     <>
+      <Card className="mb-4">
+        <div className="flex justify-center items-end">
+          <div className="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-5">
+            {category?.map((t) => (
+              <div key={t.categoryId}>
+                <Button
+                  size="sm"
+                  variant="outlined"
+                  onClick={() => onChangeCategory(t._id)}
+                >
+                  {t.name}
+                </Button>
+              </div>
+            ))}
+            <Button variant="outlined" onClick={openDrawerTop}>
+              {" "}
+              Filter{" "}
+            </Button>
+          </div>
+        </div>
+      </Card>
       <div className="flex justify-center items-end">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
           {dashboard.map((jobDetail, index) => (
@@ -70,6 +207,22 @@ const JobDashboard = () => {
           <div id="intersectionTarget"></div>
         </div>
       </div>
+      <Drawer
+        placement="top"
+        open={openTop}
+        onClose={closeDrawerTop}
+        className="p-4"
+      >
+        <div className="mb-6 flex items-center justify-between"></div>
+        <div className="flex gap-2">
+          <Button size="sm" variant="outlined">
+            Apply
+          </Button>
+          <Button size="sm" variant="outlined">
+            Cancel
+          </Button>
+        </div>
+      </Drawer>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
-
+const withMT = require("@material-tailwind/react/utils/withMT");
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -255,4 +255,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+});
